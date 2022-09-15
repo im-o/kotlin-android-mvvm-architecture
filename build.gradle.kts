9ad8584c08166e2f021dcdf1d90295b977866b7e
rivaldy <frizgt@gmail.com>
2022-09-15 13:10:12 +0800

    :arrow_up: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("com.android.tools.build:gradle:7.1.1")
         classpath("com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt_version}")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0-RC")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
